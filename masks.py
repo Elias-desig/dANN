e5cd02046c80706af63b1c37be1a8f1da80962ba
Elias Brand <eliasbrand@icloud.com>
2025-07-11 21:57:43 +0200

    slightly better global mask

--- masks.py
+++ masks.py
@@ -25,28 +25,23 @@ def rf_mask(image_size, num_dendrites, num_somas, type='local', rf_size=16):
     elif type == 'global':
         image =  np.arange(layer_size).reshape((image_size[0], image_size[1]))
         center_centers = rng.choice(layer_size, num_somas)
-        for i, center in enumerate(center_centers):
-            nb = nb_vals(image, center, rf_size // 4)
-            if len(nb) < rf_size:
-                counter = 1
-                while len(nb) < rf_size:
-                    nb = nb_vals(image, center,  (rf_size // 8) + counter)
-                    counter += 1
-            synapses = rng.choice(nb, rf_size)
-            flat_indices = np.ravel_multi_index((synapses[:, 0], synapses[:, 1]), (28, 28))
-            mask[i, flat_indices] = 1             
-
-        centers = rng.choice(layer_size, num_dendrites, replace=True)
-        for i, center in enumerate(centers):
-            nb = nb_vals(image, center, rf_size // 8)
-            if len(nb) < rf_size:
-                counter = 1
-                while len(nb) < rf_size:
-                    nb = nb_vals(image, center,  (rf_size // 8) + counter)
-                    counter += 1
-            synapses = rng.choice(nb, rf_size)
-            flat_indices = np.ravel_multi_index((synapses[:, 0], synapses[:, 1]), (28, 28))
-            mask[i, flat_indices] = 1        
+        for soma_id, c_c in enumerate(center_centers):
+            glob_nb = nb_vals(image, c_c, rf_size // 4)
+            counter = 1
+            while len(glob_nb) < connectivity:
+                glob_nb = nb_vals(image, c_c, (rf_size // 4) + counter)
+                counter += 1
+            dendrite_centers = rng.choice(glob_nb, connectivity, replace=True)
+            for i, center in enumerate(dendrite_centers):
+                nb = nb_vals(image, center, rf_size // 8)
+                if len(nb) < rf_size:
+                    counter = 1
+                    while len(nb) < rf_size:
+                        nb = nb_vals(image, center,  (rf_size // 8) + counter)
+                        counter += 1
+                synapses = rng.choice(nb, rf_size)
+                flat_indices = np.ravel_multi_index((synapses[:, 0], synapses[:, 1]), (28, 28))
+                mask[i + soma_id, flat_indices] = 1                            
     elif type == 'random':
         for i in range(num_dendrites):
             ids = rng.choice(layer_size, rf_size, replace=False)
